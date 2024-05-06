a8f397da5bd3a65b24e88861b002eb4b4910dda4
Kevin Sousselier <kevin.sousselier@gmail.com>
2024-05-06 22:37:37 +0200

    Fix proxy

--- Sample.Bff/Sample.Bff/ClientApp/src/setupProxy.js
+++ Sample.Bff/Sample.Bff/ClientApp/src/setupProxy.js
@@ -1,9 +1,9 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { legacyCreateProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
     ["/remote/*", "/bff/*", "/signin-oidc", "/signout-callback-oidc"],
-    createProxyMiddleware ({
+    legacyCreateProxyMiddleware ({
       target: "https://localhost:7265",
       secure: false,
     })
