eb390b6cf50bc219257361a1b4ee92c4335b9640
Kevin Sousselier <kevin.sousselier@gmail.com>
2024-08-25 19:30:36 +0200

    Fix visibility

--- Sample.Bff/Sample.Bff/WebClientOptions.cs
+++ Sample.Bff/Sample.Bff/WebClientOptions.cs
@@ -1,3 +1,3 @@
 ﻿namespace Sample.Bff;
 
-internal record WebClientOptions(string Authority, string ClientId, string Secret);
+public record WebClientOptions(string Authority, string ClientId, string Secret);
