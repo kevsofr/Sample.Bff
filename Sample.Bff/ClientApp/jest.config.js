59d4e7d3dcf3490231b4bcbff9e0d0dadcfd1378
Kevin Sousselier <kevin.sousselier@gmail.com>
2025-01-19 22:10:18 +0100

    Fix

--- Sample.Bff/ClientApp/jest.config.js
+++ Sample.Bff/ClientApp/jest.config.js
@@ -2,7 +2,7 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 }
--- Sample.Bff/ClientApp/vite.config.ts
+++ Sample.Bff/ClientApp/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
