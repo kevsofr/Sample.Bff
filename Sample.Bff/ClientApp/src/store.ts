5dba95741a8f69213f09937268050f63e3e15b3c
Kevin Sousselier <kevin.sousselier@gmail.com>
2026-01-31 22:14:33 +0100

    Fix

--- Sample.Bff/ClientApp/src/store.ts
+++ Sample.Bff/ClientApp/src/store.ts
@@ -2,16 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { valueApi } from "./services/valueApi";
 import { userApi } from "./services/userApi";
 import valuesUiReducer from "./features/values/valuesUiSlice"
+import { logApi } from "./services/logApi";
 
 export const store = configureStore({
     reducer: {
         [userApi.reducerPath]: userApi.reducer,
         [valueApi.reducerPath]: valueApi.reducer,
+        [logApi.reducerPath]: logApi.reducer,
         valuesUi: valuesUiReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware()
         .concat(userApi.middleware)
         .concat(valueApi.middleware)
+        .concat(logApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
